c3e7fc22b387378bd84e675779988be998882459
MarcTM <marctorresmartinez@gmail.com>
2021-01-22 19:16:21 +0100

    Capturas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and marc is my name.
+          Edit <code>src/App.js</code> and Marc.
         </p>
         <a
           className="App-link"
