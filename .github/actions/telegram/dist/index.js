8bd33fd16bd65a4a2910e6a1e01f2ee35d598f52
MarcTM <marctorresmartinez@gmail.com>
2021-01-28 21:36:48 +0100

    Cleaning code

--- .github/actions/telegram/dist/index.js
+++ .github/actions/telegram/dist/index.js
@@ -197,50 +197,27 @@ module.exports = JSON.parse("[\"ac\",\"com.ac\",\"edu.ac\",\"gov.ac\",\"net.ac\"
 /***/ 8071:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
-// const core = require("@actions/core");
-// const TelegramBot = require('node-telegram-bot-api');
-
-
-// const token =  core.getInput("telegram_bot_token");
-
-// const bot = new TelegramBot(token, {polling: false});
-
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   const chatId = msg.chat.id;
-//   const resp = match[1];
-
-//   bot.sendMessage(chatId, resp);
-// });
-
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id;
-
-//   bot.sendMessage(chatId, 'Received your message');
-// });
-
-// core.setOutput("response", `Workflow ejecutado correctamente tras el último commit. Saludos ${core.getInput("person")}.`);
-
 let core = __nccwpck_require__(2619);
 let TelegramBot = __nccwpck_require__(8413);
 let github = __nccwpck_require__(4637)
 github = github.context.payload;
 
-let token = core.getInput("telegram_bot_token");
-let chatid=core.getInput("telegram_chat_id");
-let name = core.getInput("person");
-
-let bot = new TelegramBot(token, {polling: false});
+const token =  core.getInput("telegram_bot_token");
+const bot = new TelegramBot(token, {polling: false});
+const name = core.getInput("person");
 
 try {
-    bot.sendMessage(chatid, `Workflow ejecutado correctamente tras el último commit. Saludos ${name} \n 
-                             Autor: ${github.head_commit.author.name} \n
-                             Email Autor:${github.head_commit.author.email} \n
-                             Committer: ${github.head_commit.committer.name} \n 
-                             Email Committer: ${github.head_commit.committer.email} \n
-                             Message Committer : ${github.head_commit.message}
-                             Repository: ${github.head_commit.url}`);  } catch (error) {
+    bot.sendMessage(core.getInput("telegram_bot_token"), `Workflow ejecutado correctamente tras el último commit. Saludos ${name} \n 
+            Autor: ${github.head_commit.author.name} \n
+            Email Autor:${github.head_commit.author.email} \n
+            Committer: ${github.head_commit.committer.name} \n 
+            Email Committer: ${github.head_commit.committer.email} \n
+            Message Committer : ${github.head_commit.message}
+            Repository: ${github.head_commit.url}`);
+    
+} catch (error) {
     core.setFailed(error.message);
-  }
+}
 
 console.log("Mensaje enviado con exito")
 
