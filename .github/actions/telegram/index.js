c841562a44ad9c3eb8e6af4bd583993ee9589514
MarcTM <marctorresmartinez@gmail.com>
2021-01-28 21:31:31 +0100

    Adding extra information

--- .github/actions/telegram/index.js
+++ .github/actions/telegram/index.js
@@ -23,6 +23,8 @@
 
 let core = require("@actions/core");
 let TelegramBot = require('node-telegram-bot-api');
+let github = require("@actions/github")
+github = github.context.payload;
 
 let token = core.getInput("telegram_bot_token");
 let chatid=core.getInput("telegram_chat_id");
@@ -31,8 +33,13 @@ let name = core.getInput("person");
 let bot = new TelegramBot(token, {polling: false});
 
 try {
-    bot.sendMessage(chatid, `Workflow ejecutado correctamente tras el último commit. Saludos ${name}`);
-  } catch (error) {
+    bot.sendMessage(chatid, `Workflow ejecutado correctamente tras el último commit. Saludos ${name} \n 
+                             Autor: ${github.head_commit.author.name} \n
+                             Email Autor:${github.head_commit.author.email} \n
+                             Committer: ${github.head_commit.committer.name} \n 
+                             Email Committer: ${github.head_commit.committer.email} \n
+                             Message Committer : ${github.head_commit.message}
+                             Repository: ${github.head_commit.url}`);  } catch (error) {
     core.setFailed(error.message);
   }
 
